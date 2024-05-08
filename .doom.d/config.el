868846a762338581304a0b9744d727579eaae611
Benjamin Miller <benmiller179@gmail.com>
2024-05-07 19:23:01 -0500

    Trello one-way sync hack

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -202,6 +202,19 @@
 
 (setq neo-smart-open t)
 
+;;;###autoload
+(defun org-trello-pull-buffer (&optional from)
+  "Execute the sync of the entire buffer to trello.
+If FROM is non nil, execute the sync of the entire buffer from trello."
+  (interactive "P")
+  (org-trello--apply-deferred
+   (cons 'org-trello-log-strict-checks-and-do
+         (if from
+             '("Request 'sync org buffer from trello board'"
+               orgtrello-controller-do-sync-buffer-from-trello)
+           '("Request 'sync org buffer from trello board'"
+             orgtrello-controller-do-sync-buffer-from-trello)))))
+
 (after! org
   (add-to-list 'org-capture-templates
                '("i" "Inbox item" entry
